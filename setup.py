87f14db93a1bcb41d1e076a99f0c2362b7cdb819
AymerickStage <65296563+AymerickStage@users.noreply.github.com>
2020-07-30 14:07:38 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = 'ScopeFoundryHW.Andor_camera',
+    
+    version = '0.0.1',
+    
+    description = 'ScopeFoundry Hardware plug-in: Andor camera',
+    
+    # Author details
+    author='Edward S. Barnard',
+    author_email='esbarnard@lbl.gov',
+
+    # Choose your license
+    license='BSD',
+
+    package_dir={'ScopeFoundryHW.andor_camera': '.'},
+    
+    packages=['ScopeFoundryHW.andor_camera',],
+        
+    package_data={
+        '':["README*", 'LICENSE', # include License and readme 
+            "*.ui", # include QT ui files 
+            ], 
+        },
